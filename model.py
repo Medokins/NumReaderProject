aa5963a514f08eb83858ca4e21feff6d0ffbfe80
Nikodem <nikodemszwast@gmail.com>
2023-01-07 14:11:32 +0100

    add model.py

--- model.py
+++ model.py
@@ -8,15 +8,15 @@ def trainNumReader():
     x_test = tf.keras.utils.normalize(x_test, axis=1)
 
     model = tf.keras.models.Sequential()
-    model.add(tf.keras.layers.Flatten(input_shape = (28, 28)))   #Flatten the images! Could be done with numpy reshape
+    model.add(tf.keras.layers.Flatten(input_shape = (28, 28)))
     model.add(tf.keras.layers.Dense(128, activation=tf.nn.relu, input_shape= x_train.shape[1:]))
     model.add(tf.keras.layers.Dense(128, activation=tf.nn.relu))
-    model.add(tf.keras.layers.Dense(10, activation=tf.nn.softmax))   #10 because dataset is numbers from 0 - 9
+    model.add(tf.keras.layers.Dense(10, activation=tf.nn.softmax))  #10 because dataset is numbers from 0 - 9
 
 
-    model.compile(optimizer='adam',  # Good default optimizer to start with
-              loss='sparse_categorical_crossentropy',  # how will we calculate our "error." Neural network aims to minimize loss.
-              metrics=['accuracy'])  # what to track
+    model.compile(optimizer='adam', 
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
 
     # training
     model.fit(x_train, y_train, validation_data = (x_test, y_test), epochs=EPOCHS)
